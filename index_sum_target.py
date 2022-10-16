398b541cf98965bc6ded89a4f663794ce01a2da8
bapata <bapata@ymail.com>
2022-10-16 10:13:50 -0700

    Create index_sum_target.py

---
+++ index_sum_target.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# returns index in a list whose contents when added results in specified number
+
+# Examples:
+# list=[1,4,3,8], target=11, result=(2,3)
+# list=[6,5,3,7,9,11,1], target=16, result=(3,4)
